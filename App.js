a5be8dd69ac0c349a3f32b22a28d73fac12057cc
James Kip <jameskip@me.com>
2017-06-24 17:04:39 -0500

    Add fetch request for weather data

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    return fetch('https://api.darksky.net/forecast/254ee42b02caecd90d8cb312d885b884/42.3601,-71.0589,409467600')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch(error => console.error(error))
+  }
+
   render() {
     return (
       <View style={styles.container}>
